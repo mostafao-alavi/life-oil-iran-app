72b77e5dcf33d6bddf2453a2f162451707c985f5
Mostafao alavi <paktia96@gmail.com>
2026-07-22 03:52:11 +0330

    build: update open-next build process
    - Remove redundant internal service reference in wrangler config
    - Simplify build, deploy, and preview scripts
    - Update internal open-next build artifacts

---
+++ .open-next/assets/_next/static/chunks/app/not-found-7e91a3ffe722d802.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[345],{1435:(e,n,s)=>{Promise.resolve().then(s.bind(s,3293))},3293:(e,n,s)=>{"use strict";s.r(n),s.d(n,{default:()=>u});var r=s(5155);function u(){return(0,r.jsx)("div",{children:"404 Not Found"})}}},e=>{e.O(0,[441,255,358],()=>e(e.s=1435)),_N_E=e.O()}]);
